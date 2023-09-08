6827507a5a29d942f48c7f03570e0a6133e8baab
Stella <ellastream31@gmail.com>
2023-09-07 18:14:36 -0700

    miliseconds time added

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ console.log(utcTime);
 
 let hours = date.getHours();
 let minutes = date.getMinutes();
+let miliseconds = date.getMilliseconds();
 
 let days = ["SUN", "MON", "TUE", "WED", "THUR", "FRI", "SAT"];
 let day = days[date.getDay()];
@@ -12,4 +13,4 @@ let day = days[date.getDay()];
 //document.querySelector("#currentUTCTime").innerHTML = `${hours}:${minutes}`;
 
 document.querySelector('[data-testid="currentDayOfTheWeek"]').innerHTML = `${day}`;
-document.querySelector('[data-testid="currentUTCTime"]').innerHTML = `${hours}:${minutes}`;
+document.querySelector('[data-testid="currentUTCTime"]').innerHTML = `${hours}:${minutes}:${miliseconds}`;
