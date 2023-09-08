7252fcd673b6848d3542787130137ec41faec4e3
Stella <ellastream31@gmail.com>
2023-09-07 19:02:51 -0700

    changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let date = new Date();
+/*let date = new Date();
 let utcTime = date.toLocaleString('en-US', { timeZone: 'UTC'});
 console.log(utcTime);
 
@@ -7,10 +7,14 @@ let minutes = date.getMinutes();
 let miliseconds = date.getMilliseconds();
 
 let days = ["SUN", "MON", "TUE", "WED", "THUR", "FRI", "SAT"];
-let day = days[date.getDay()];
+let day = days[date.getDay()];*/
 
 //document.querySelector("#currentDayOfTheWeek").innerHTML = `${day}`;
 //document.querySelector("#currentUTCTime").innerHTML = `${hours}:${minutes}`;
 
+const hours = Math.floor(miliseconds / (1000 * 60 * 60));
+const minutes =Math.floor((miliseconds % (1000 * 60 * 60))/ (1000 * 60)); 
+const seconds = Math.floor((miliseconds % (1000 * 60 ))/ 1000 ); 
+
 document.querySelector('[data-testid="currentDayOfTheWeek"]').innerHTML = `${day}`;
-document.querySelector('[data-testid="currentUTCTime"]').innerHTML = `${hours}:${minutes}:${miliseconds}`;
+document.querySelector('[data-testid="currentUTCTime"]').innerHTML = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
